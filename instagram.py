0fc712acd7ae26f8b60c317e28e988ad8f784ca8
IntiMech <zacmconsulting@gmail.com>
2023-10-17 10:06:25 +1100

    First Commit

---
+++ instagram.py
@@ -0,0 +1,131 @@
+from uiautomator import device as d
+import re
+from utils import random_wait
+
+class InstagramActions:
+    def __init__(self):
+        pass
+        
+    def open_instagram(self):
+        d.press.home()
+        d(text="Instagram").click()
+
+    def close_instagram(self):
+        d.press.home()
+
+    def navigate_to_home_feed(self):
+        d(resourceId='com.instagram.android:id/feed_tab').click()
+
+class CommentInteractions:
+    def __init__(self):
+        super().__init__()
+        self.processed_comments = set()
+        
+    def scroll_until_comment_section_visible(self, max_tries=10):
+        tries = 0
+        while not self.find_comment_section() and tries < max_tries:
+            d.swipe(525, 1105, 565, 625, steps=30)
+            random_wait(1, 2)
+            tries += 1
+
+    def get_total_comments(self):
+        comment_section = self.find_comment_section()
+        if comment_section:
+            text = comment_section.text
+            match = re.search(r'(\d+)', text)
+            if match:
+                return int(match.group(1))
+        return 0
+
+    def open_comment_section(self):
+        comment_section = self.find_comment_section()
+        if comment_section:
+            comment_section.click()
+            random_wait(3, 4)
+
+    def capture_comments(self, batches=1, liked_count=0, max_likes=None):
+        all_comments = []
+        previous_comments = set()
+        repetitive_count = 0
+
+        for _ in range(batches):
+            comments = []
+
+            if not self.is_in_comment_section():
+                self.recover_from_disruption()
+
+            for attempt in range(3):
+                try:
+                    comment_elements = d(resourceId='com.instagram.android:id/row_comment_textview_comment')
+                    for element in comment_elements:
+                        if element.text not in self.processed_comments:
+                            comments.append((element.text, element))
+                            self.processed_comments.add(element.text)
+
+                    liked_count = self.like_comments_based_on_criteria(comments, liked_count, max_likes)
+
+                    current_comments = set([comment[0] for comment in comments])
+                    if current_comments == previous_comments:
+                        repetitive_count += 1
+                        if repetitive_count >= 2:
+                            return all_comments
+                    else:
+                        repetitive_count = 0
+
+                    previous_comments = current_comments
+                    break
+
+                except Exception:  # UIautomator doesn't have StaleElementReferenceException, so a general exception is used
+                    print("Encountered an error. Re-finding elements...")
+                    comment_elements = d(resourceId='com.instagram.android:id/row_comment_textview_comment')
+
+            d.swipe(500, 1165, 515, 680, steps=30)
+
+        return all_comments
+
+    def like_comments_based_on_criteria(self, comments, liked_count, max_likes):
+        keywords = ["😊", "👍", "❤️", "🥰", "😘", "😍", "🤤"]
+        for comment_text, comment_element in comments:
+            if liked_count >= max_likes:
+                return liked_count
+
+            for keyword in keywords:
+                if keyword in comment_text:
+                    self.like_comment(comment_element)
+                    liked_count += 1
+                    break
+        return liked_count
+
+    def like_comment(self, comment_element):
+        try:
+            all_comment_elements = d(resourceId='com.instagram.android:id/row_comment_textview_comment')
+            all_like_buttons = d(resourceId='com.instagram.android:id/row_comment_like_button_click_area')
+
+            comment_index = all_comment_elements.index(comment_element)
+
+            like_button = all_like_buttons[comment_index]
+            like_button.click()
+            print(f"Successfully liked the comment: '{comment_element.text}'.")
+
+        except ValueError:
+            print(f"Error: The comment '{comment_element.text}' is not in the current view")
+
+        except Exception:  # Catching general exceptions in case the like button isn't found or any other issues arise
+            print(f"Error: Like button not found for the comment: '{comment_element.text}'.")
+
+    def close_comment_section(self):
+        d.press.back()
+        
+    def is_in_comment_section(self):
+        return d.exists(resourceId='com.instagram.android:id/layout_comment_thread_edittext')
+
+    def recover_from_disruption(self):
+        d.press.back()
+        random_wait(1, 2)
+        if not self.is_in_comment_section():
+            print("Recovery Failed. Restarting sequence for this post.")
+            self.close_comment_section()
+            self.scroll_until_comment_section_visible()
+            self.open_comment_section()
+
+
---
+++ main.py
@@ -0,0 +1,67 @@
+from uiautomator import device as d
+from instagram import InstagramActions, CommentInteractions  # Assuming you renamed the file after translating to UIautomator
+from utils import random_wait
+import random
+
+
+try:
+    instagram_actions = InstagramActions()
+
+    instagram_actions.open_instagram()
+    random_wait(3)
+
+    comment_interactions = CommentInteractions()
+
+    for _ in range(3):  # This loop ensures the comment section is found and comments are liked three times
+    # Reset processed comments at the start of each iteration
+        comment_interactions.processed_comments.clear()
+
+        # Scroll until the comment section is visible
+        comment_interactions.scroll_until_comment_section_visible()
+        random_wait(1)
+
+        total_comments = comment_interactions.get_total_comments()
+        print(f"Total Number of comments: {total_comments}")
+
+        comments_to_like = min(50, random.randint(int(0.5 * total_comments)))
+        print(f"Number of comments to like: {comments_to_like}")
+
+        comment_interactions.open_comment_section()
+        random_wait(2)
+
+        liked_so_far = 0
+        consecutive_empty_batches = 0
+        max_consecutive_empty = 2
+
+        # Capture and like comments in small batches multiple times
+        for _ in range(5):  # Adjust the range as needed
+            comment_data = comment_interactions.capture_comments(batches=4, liked_count=liked_so_far, max_likes=comments_to_like)
+            print("Captured comments:", [comment[0] for comment in comment_data])
+
+            if not comment_data:
+                consecutive_empty_batches += 1
+                if consecutive_empty_batches >= max_consecutive_empty:
+                    print("Detected hidden or inaccessible comments. Moving on...")
+                    break
+            else:
+                consecutive_empty_batches = 0  # Reset if there are comments in the batch
+
+            liked_so_far += len([comment for comment in comment_data if comment[0] in comment_interactions.processed_comments])
+            random_wait(2)
+
+            if liked_so_far >= comments_to_like:
+                break
+        
+        comment_interactions.close_comment_section()
+        print(f"Comment Section Closed!")
+        random_wait(3)
+
+    instagram_actions.navigate_to_home_feed()
+    print(f"Navigated Home!")
+    random_wait(2)
+
+except Exception as e:
+    print('Error:', e)
+
+
+
