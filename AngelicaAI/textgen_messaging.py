013b0c57f026b9273575d2499806010ad980244a
IntiMech <zacmconsulting@gmail.com>
2023-11-24 15:31:50 +1100

    aider: Replace hardcoded URL with a call to `os.getenv()` to retrieve the URL from the environment variables.
    # Aider chat conversation:
    
    USER: can you please change the file back to pull the url from the .env file where I have the textgen url saved
    
    ASSISTANT: To make this change we need to modify `AngelicaAI/textgen_messaging.py` to:
    
    1. Replace the hardcoded URL with a call to `os.getenv()` to retrieve the URL from the environment variables.
    
    Here is the *SEARCH/REPLACE* block:
    
    AngelicaAI/textgen_messaging.py
    ```python
    <<<<<<< SEARCH
    self.TEXTGEN_API_URL = "http://127.0.0.1:5000/v1/chat/completions"
    =======
    self.TEXTGEN_API_URL = os.getenv("TEXTGEN_API_URL")
    >>>>>>> REPLACE
    ```
    
    This change will make the `TextgenMessaging` class retrieve the URL for the text generation API from the environment variables, which are set in your .env file.

--- AngelicaAI/textgen_messaging.py
+++ AngelicaAI/textgen_messaging.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 class TextgenMessaging:
     def __init__(self):
-        self.TEXTGEN_API_URL = "http://127.0.0.1:5000/v1/chat/completions"
+        self.TEXTGEN_API_URL = os.getenv("TEXTGEN_API_URL")
         self.HEADERS = {
             "Content-Type": "application/json"
         }
