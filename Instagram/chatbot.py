e530c206e5ce1d2c2774d4f3de0e656101a9a3de
IntiMech <zacmconsulting@gmail.com>
2023-12-03 08:46:11 +1100

    Add Instagram chatbot class with basic methods and sample usage.

---
+++ Instagram/chatbot.py
@@ -0,0 +1,19 @@
+from uiautomator import device as d
+import random
+import re
+import logging
+from .utils import random_sleep
+from time import time
+from AngelicaAI.textgen_messaging import TextgenMessaging
+from AngelicaAI.supabase_handler import SupabaseHandler
+from .services import InstagramActions
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+class InstagramChatbot:
+    # ... all methods of InstagramChatbot ...
+
+if __name__ == "__main__":
+    bot = InstagramChatbot()
+    # Sample usage or testing can be added here
