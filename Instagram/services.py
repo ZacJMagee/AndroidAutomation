22fae61980fc021a59998721d8184accfc276b9d
IntiMech <zacmconsulting@gmail.com>
2023-12-03 08:25:53 +1100

    Add InstagramActions class with methods for performing actions on Instagram.

---
+++ Instagram/services.py
@@ -0,0 +1,11 @@
+from uiautomator import device as d
+import random
+import re
+import logging
+from .utils import random_sleep
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+class InstagramActions:
+    # ... all methods of InstagramActions ...
